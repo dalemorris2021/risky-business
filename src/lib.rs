05fa104d036eba225e27355a95f57eb0b8f2de05
Dale Morris <dalemorris2021@gmail.com>
2025-04-10 23:39:28 -0400

    Added command line parsing and a run function

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// Number of players
+    #[arg(short, long)]
+    num_players: u32,
+}
+
+/// Contains the configuration for running this program
+#[derive(Debug)]
+pub struct Config {
+    player_count: PlayerCount,
+}
+
+impl Config {
+    pub fn build(args: Args) -> Result<Self, &'static str> {
+        let player_count = match PlayerCount::try_from(args.num_players) {
+            Ok(count) => count,
+            Err(reason) => return Err(reason),
+        };
+
+        Ok(Config { player_count })
+    }
+}
+
+/// Contains the number of players
+#[derive(Debug)]
+struct PlayerCount {
+    value: u32,
+}
+
+impl TryFrom<u32> for PlayerCount {
+    type Error = &'static str;
+
+    fn try_from(count: u32) -> Result<Self, Self::Error> {
+        if count < 2 || count > 6 {
+            return Err("PlayerCount only accepts values between 2 and 6");
+        }
+
+        Ok(PlayerCount { value: count })
+    }
+}
+
+/// Runs this program with the given configuration
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    println!("num-players = {}", config.player_count.value);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::process;
+
+use clap::Parser;
+use risky_business::{Args, Config};
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let config = Config::build(args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = risky_business::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
